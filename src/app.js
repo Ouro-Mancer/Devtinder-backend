17259a85d6db3f8c38e432bb05079a27bc4392ee
Ouro-Mancer <mayankkumardas12@gmail.com>
2025-07-22 14:49:27 +0530

    Created express server and made request handlers for different routes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,19 @@ import express from 'express';
 
 const app = express();
 
-app.use((req, res) => {
-    res.send("Hello Node js...!");
+/*
+app.use( (req, res) => {
+    res.send("Hello Node js...!");  
+    
+    //==> response handler for / route , it will only handle the requests for / route , writing route for "/" handler and not writing "/" is one and the same and one more thing Using app.use() on / first can result in all other routes being ignored depending on logic — because app.use() matches everything that starts with /.
+})
+*/
+
+app.use("/home", (req, res) => {
+    res.send("Hello Node js home...!");  //==> response handler for /home route , it will only handle the requests for /home route
+})
+app.use("/about", (req, res) => {
+    res.send("Hello Node js from about...!");  //==> response handler for /about route , it will only handle the requests for /about route
 })
 
 app.listen(3000, () => {
