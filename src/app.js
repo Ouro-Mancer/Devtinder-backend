0b48f312100d1a8527f3874baa6b104c287c7fcf
Ouro-Mancer <mayankkumardas12@gmail.com>
2025-07-23 13:09:02 +0530

    exploring the routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ import express from 'express';
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Mayank", lastName: "Kumar Das" })
+});
+
+app.post("/user", (req, res) => {
+    res.send("Data sent to database successfully..!!")
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted from database successfully..!!")
+});
 
 app.listen(3000, () => {
     console.log("Server is listening to PORT 3000..!");
